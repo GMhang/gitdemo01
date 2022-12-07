bf581c791fe707c0ae286c060e4f6a7c88f2974c
GM <614396761@qq.com>
2022-12-07 22:05:19 +0800

    idea first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author GM
+ * @date 2022/9/14 23:14
+ * @discribe
+ */
+public class Hello {
+    public void sayHello(){
+        System.out.println("hello,maven in idea!");
+    }
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @author GM
+ * @date 2022/9/14 23:15
+ * @discribe
+ */
+public class HelloTest{
+    @Test
+    public void test(){
+        new Hello().sayHello();
+    }
+
+}
